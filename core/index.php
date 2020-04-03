ee894df30f1f20ba3c7eafd2f6da6fc022c0a112
cmena <cmena7512@gmail.com>
2020-04-03 13:42:01 -0400

    problemas con la sucursal resueltos

--- core/index.php
+++ core/index.php
@@ -4,30 +4,28 @@ if(isset($_POST['secret']) && $_POST['secret'] == 'qhj651uqq6766'){
     require_once('conn.php');
 
     $con = new Conexion();
-    print_r($_POST);
     $vat = $_POST['vat'];
     $name = strtoupper($_POST['name']);
     $email = $_POST['email'];
-    $company_id = $_POST['meal_preference'];
+    $company_id = $_POST['sucursal'];
     $taller = $_POST['taller'];
     $inspirator = $_POST['inspirator'];
     $inspirator_n = $_POST['inspirator_n'];
     $phone_number = $_POST['phone_number'];
     $taller_id = $con->getRecord('product.product', array('id'), 'default_code', $taller);
-
+   
     $datos = array(
         'vat' => $vat,
         'name' => $name,
         'email' => $email,
         'company_id' => $company_id,
-        'taller_id' => $taller_id['id'],
+        'taller_id' => $taller_id[0]['id'],
         'comment' => "Nombre: ".$inspirator." numero: ".$inspirator_n,
         'mobile' => $phone_number,
        
     );
     $created = $con->create('res.partner', $datos);
-
-    if($created){
+    if(gettype($created) == integer ){
         header("Location: http://localhost:8080/odooWebService/thanks.html");
         exit();
     }else{
@@ -36,7 +34,6 @@ if(isset($_POST['secret']) && $_POST['secret'] == 'qhj651uqq6766'){
     }
 
 }else{
-    print($_POST['secret']);
-    print($_POST['name']);
-    print("estoy en el else");
+    header("Location: http://localhost:8080/odooWebService");
+    exit();
 }
